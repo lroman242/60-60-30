c30ed55588211e27c17c1232ed365415668a43bc
Roman <lroman242@gmail.com>
2020-05-28 19:15:21 +0300

    add trait buttons

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.14
 require (
 	github.com/faiface/beep v1.0.2
 	github.com/gen2brain/beeep v0.0.0-20200420150314-13046a26d502
+	github.com/getlantern/systray v1.0.2
 )
--- main.go
+++ main.go
@@ -3,14 +3,11 @@ package main
 import (
 	"context"
 	"fmt"
-	"github.com/faiface/beep"
-	"github.com/faiface/beep/mp3"
-	"github.com/faiface/beep/speaker"
+	//"github.com/faiface/beep"
+	//"github.com/faiface/beep/mp3"
+	//"github.com/faiface/beep/speaker"
 	"github.com/gen2brain/beeep"
-	"log"
-	"os"
-	"os/signal"
-	"syscall"
+	"github.com/getlantern/systray"
 	"time"
 )
 
@@ -19,81 +16,37 @@ const WORK_DURATION_MIN = 25
 const SHORT_BREAK_DURATION_MIN = 5
 const FINAL_BREAK_DURATION = 30
 
+//var audioStream beep.StreamSeekCloser
+var isAudioEnabled bool = false
+var isAutoRestartEnabled bool = false
+
 func main() {
-	f, err := os.Open("assets/beep.mp3")
-	if err != nil {
-		log.Fatal(err)
-	}
+	//f, err := os.Open("assets/beep.mp3")
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//
+	//audioStream, format, err := mp3.Decode(f)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//fmt.Println(audioStream)
+	//defer audioStream.Close()
+	//speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
 
-	streamer, format, err := mp3.Decode(f)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer streamer.Close()
-	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
 
-	ctx := context.Background()
-	ctxWithCancel, cancelFunction := context.WithCancel(ctx)
-
-	// Clean all resources
-	defer func() {
-		fmt.Println("main: canceling context")
-		cancelFunction()
-	}()
-
-	// define state change what will be used to stop and start 60-60-30 working cycles
-	stateChan := make(chan int, 1)
-
-	// state changes handler
-	go func(ctx context.Context, stateChan chan int) {
-		ctxWithCancel, cancelFunction := context.WithCancel(ctx)
-
-		// clean all resources
-		defer func() {
-			fmt.Println("handler: canceling context")
-			cancelFunction()
-		}()
-
-		for {
-			select {
-			// handle cancel from main
-			case <-ctx.Done():
-				break
-			// handle state changes
-			case state := <-stateChan:
-				// start cycle
-				if state == 1 {
-					fmt.Println("start new cycle")
-					go startCycle(ctxWithCancel, streamer)
-				} else if state == 0 { // stop cycle
-					fmt.Println("Stop!")
-					cancelFunction()
-
-					// previous context has already been canceled, so new
-					// context required to have ability to start new working cycle
-					ctxWithCancel, cancelFunction = context.WithCancel(ctx)
-				}
-			}
-		}
-	}(ctxWithCancel, stateChan)
-
-	//TODO: trait icon + trait menu handler with start, stop and exit
-	stateChan <- 1
-
-	// awaiting to exit signal
-	ch := make(chan os.Signal, 1)
-	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
-	s := <-ch
-	log.Printf("Got signal: %v, exiting.", s)
+	systray.Run(onReady, func() {
+		fmt.Println("onExitHandler")
+	})
 }
 
-func startCycle(ctx context.Context, streamer beep.StreamSeekCloser) {
+// Run working cycle
+func startCycle(ctx context.Context) {
 	finalState := NUM_OF_WORK_PERIODS * 2
 
 	for state := 1; state <= finalState; state++ {
-		streamer.Seek(0)
-		speaker.Play(beep.Seq(streamer, beep.Callback(func() {})))
-
+		fmt.Println(state)
+		//playBeep()
 		if state == finalState {
 			err := beeep.Alert("60-60-30 ЦИКЛЫ", "Отдохни 30мин", "assets/warning.png")
 			if err != nil {
@@ -107,7 +60,7 @@ func startCycle(ctx context.Context, streamer beep.StreamSeekCloser) {
 				return
 			case <-time.After(FINAL_BREAK_DURATION * time.Second):
 				// do nothing
-				//fmt.Printf("Final state %v finished\n", state)
+				//TODO: auto restart option + 3 x playBeep
 			}
 		} else if state%2 == 0 { // short break period
 			err := beeep.Alert("60-60-30 ЦИКЛЫ", "Отдохни 5мин", "assets/warning.png")
@@ -122,7 +75,6 @@ func startCycle(ctx context.Context, streamer beep.StreamSeekCloser) {
 				return
 			case <-time.After(SHORT_BREAK_DURATION_MIN * time.Second):
 				// do nothing. go to next period
-				//fmt.Printf("Break state %v finished\n", state)
 			}
 		} else { // work period
 			err := beeep.Alert("60-60-30 ЦИКЛЫ", "Начался рабочий цикл в 25мин", "assets/warning.png")
@@ -140,4 +92,116 @@ func startCycle(ctx context.Context, streamer beep.StreamSeekCloser) {
 			}
 		}
 	}
+}
+
+//func playBeep() {
+//	fmt.Println(audioStream)
+//
+//	audioStream.Seek(0)
+//	speaker.Play(beep.Seq(audioStream, beep.Callback(func() {})))
+//}
+
+func onReady() {
+	ctx := context.Background()
+	ctxWithCancel, _ := context.WithCancel(ctx)
+
+	systray.SetTemplateIcon(Icon, Icon)
+	systray.SetIcon(Icon)
+	systray.SetTooltip("60-60-30 work cycle")
+
+	btnStart := systray.AddMenuItem("Start", "Start Cycle")
+	btnStop := systray.AddMenuItem("Stop", "Stop Current Cycle")
+	btnRestart := systray.AddMenuItem("Restart", "Restart Cycle")
+	systray.AddSeparator()
+	btnEnableAutoRestart := systray.AddMenuItem("Enable Auto Restart", "Enable Auto Restart")
+	btnDisableAutoRestart := systray.AddMenuItem("Disable Auto Restart", "Disable Auto Restart")
+
+	btnEnableAudio := systray.AddMenuItem("Enable Audio Signal", "Enable Audio Signal")
+	btnDisableAudio := systray.AddMenuItem("Disable Audio Signal", "Enable Audio Signal")
+	systray.AddSeparator()
+	btnQuit := systray.AddMenuItem("Quit", "Quit the whole app")
+
+	go func(ctx context.Context) {
+		ctxWithCancel, cancelFunc := context.WithCancel(ctx)
+		defer cancelFunc()
+
+		btnStart.SetIcon(StartIcon)
+		btnStart.Enable()
+
+		btnStop.SetIcon(StopIcon)
+		btnStop.Disable()
+
+		btnRestart.SetIcon(RestartIcon)
+		btnRestart.Disable()
+
+		if isAudioEnabled {
+			btnEnableAudio.Hide()
+			btnDisableAudio.Show()
+		} else {
+			btnEnableAudio.Show()
+			btnDisableAudio.Hide()
+		}
+
+		if isAutoRestartEnabled {
+			btnEnableAutoRestart.Hide()
+			btnDisableAutoRestart.Show()
+		} else {
+			btnEnableAutoRestart.Show()
+			btnDisableAutoRestart.Hide()
+		}
+
+		for {
+			select {
+			// handle cancel from main
+			case <-ctx.Done():
+				cancelFunc()
+				break
+			case <-btnStart.ClickedCh:
+				btnStart.Disable()
+				btnStop.Enable()
+				btnRestart.Enable()
+
+				go startCycle(ctxWithCancel)
+			case <-btnStop.ClickedCh:
+				btnStart.Enable()
+				btnStop.Disable()
+				btnRestart.Disable()
+
+				cancelFunc()
+
+				// previous context has already been canceled, so new
+				// context required to have ability to start new working cycle
+				ctxWithCancel, cancelFunc = context.WithCancel(ctx)
+			case <-btnRestart.ClickedCh:
+				//Stop existed
+				cancelFunc()
+
+				// previous context has already been canceled, so new
+				// context required to have ability to start new working cycle
+				ctxWithCancel, cancelFunc = context.WithCancel(ctx)
+
+				//Run new
+				go startCycle(ctxWithCancel)
+			case <-btnEnableAudio.ClickedCh:
+				isAudioEnabled = true
+				btnEnableAudio.Hide()
+				btnDisableAudio.Show()
+			case <-btnDisableAudio.ClickedCh:
+				isAudioEnabled = false
+				btnEnableAudio.Show()
+				btnDisableAudio.Hide()
+			case <-btnEnableAutoRestart.ClickedCh:
+				isAutoRestartEnabled = true
+				btnEnableAutoRestart.Hide()
+				btnDisableAutoRestart.Show()
+			case <-btnDisableAutoRestart.ClickedCh:
+				isAutoRestartEnabled = false
+				btnEnableAutoRestart.Show()
+				btnDisableAutoRestart.Hide()
+			case <-btnQuit.ClickedCh:
+				systray.Quit()
+				return
+			}
+		}
+	}(ctxWithCancel)
 }
